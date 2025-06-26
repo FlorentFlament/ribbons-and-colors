ea88be097ebc805e24161f18bac0f1d1b998c781
Florent Flament <contact@florentflament.com>
2025-06-26 09:07:42 +0200

    Removing an unused variable

--- src/text_variables.asm
+++ src/text_variables.asm
@@ -1,4 +1,4 @@
-sprite_it  = ptr0               ; Counter to display 1 sprite
+dummy  = ptr0               ; Counter to display 1 sprite
 sprite_cnt = ptr0 + 1           ; Counter to display 1 column of sprites
 
 sp0_pos         ds.b    1               ; storage for sprite 0 fine positionning
