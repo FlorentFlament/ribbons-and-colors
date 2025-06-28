021f41bd7889ae7ed9f9d70b5e2a99ad901a56f8
Florent Flament <contact@florentflament.com>
2025-06-28 19:06:32 +0200

    Add sine generation tooling

---
+++ tools/gen-sine.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from math import sin, pi
+from asmlib import *
+AMPLITUDE = 32
+OFFSET = 0
+
+# -0.5 to have rounded value between 0 and AMPLITUDE-1 (ignoring OFFSET)
+f = [round((1+sin(2*pi*x/256.0))*AMPLITUDE/2 - 0.5)+OFFSET for x in range(256)]
+print(lst2asm(f))
+
