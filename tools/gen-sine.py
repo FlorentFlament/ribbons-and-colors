7641bbf0e35a0a2ba0be3a0c348df043d0152383
Florent Flament <contact@florentflament.com>
2025-06-28 19:44:34 +0200

    Smoother sine

--- tools/gen-sine.py
+++ tools/gen-sine.py
@@ -5,6 +5,12 @@ AMPLITUDE = 32
 OFFSET = 0
 
 # -0.5 to have rounded value between 0 and AMPLITUDE-1 (ignoring OFFSET)
-f = [round((1+sin(2*pi*x/256.0))*AMPLITUDE/2 - 0.5)+OFFSET for x in range(256)]
+f = [round( \
+            ( (1 + sin(2*pi*x / 256.0)) \
+              * AMPLITUDE/2 - 0.5
+            ) * 0.999 \
+           ) + OFFSET \
+     for x in range(256)]
+
 print(lst2asm(f))
 
