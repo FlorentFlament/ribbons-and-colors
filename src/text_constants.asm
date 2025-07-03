2aa1c35de2ebff19f78f920f5238600e30766f80
Florent Flament <contact@florentflament.com>
2025-07-03 13:16:07 +0200

    Add fx transitions before looping

--- src/text_constants.asm
+++ src/text_constants.asm
@@ -4,4 +4,4 @@ CHARACTERS_COUNT    = 15
 BORDER_HEIGHT       = 26
 TOTAL_SPRITE_HEIGHT = 11        ; 8 lines of chars + 3 positionning
 TRACK_PATTERN_FRAMES = 224
-FX_MAX_INDEX = 8
+FX_MAX_INDEX = 10
