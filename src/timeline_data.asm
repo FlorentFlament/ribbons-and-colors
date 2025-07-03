9cd0ad11fdcfd9c571e968eba9f423a987629d2a
Florent Flament <contact@florentflament.com>
2025-07-03 13:14:30 +0200

    Add sync screen

--- src/timeline_data.asm
+++ src/timeline_data.asm
@@ -1,11 +1,11 @@
 timeline_vblanks:
-;    .word   sync_vblank-1
+    .word   sync_vblank-1
     .word   text_vblank-1
 
 timeline_kernels:
-;    .word   sync_kernel-1
+    .word   sync_kernel-1
     .word   text_kernel-1
 
 timeline_overscans:
-;    .word   sync_overscan-1
+    .word   sync_overscan-1
     .word   text_overscan-1
